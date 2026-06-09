66156e1328b8add0bc128c79d25d4260321ede24
kasobi <nicholaskasbar@gmail.com>
2026-06-09 16:19:12 -0700

    added reference to /docs for GitPages in build.sh

--- src/f_recursive_generate_pages.py
+++ src/f_recursive_generate_pages.py
@@ -1,16 +1,15 @@
 from f_markdown_to_html_node import markdown_to_html_node
 from f_extract_title import extract_title
-from f_recursive_static_to_private import r_static_to_private
 import os
 
 
-def r_generate_pages(from_path, template_path, dest_path):
+def r_generate_pages(basepath, from_path, template_path, dest_path):
     if os.path.isdir(from_path):
         if not os.path.exists(dest_path):
             os.mkdir(dest_path)
             print(f"made directory: {dest_path}")
         for path in os.listdir(from_path):
-            r_generate_pages(os.path.join(from_path, path), template_path, os.path.join(dest_path, path))
+            r_generate_pages(basepath, os.path.join(from_path, path), template_path, os.path.join(dest_path, path))
             
     else:
         print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -25,6 +24,8 @@ def r_generate_pages(from_path, template_path, dest_path):
 
         new_page = template_content.replace("{{ Title }}", title)
         new_page = new_page.replace("{{ Content }}", html_from_origin)
+        new_page = new_page.replace('href="/', f'href={basepath}')
+        new_page = new_page.replace('src="/', f'href={basepath}')
 
         dest_path = os.path.splitext(dest_path)[0] + ".html"
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
--- src/main.py
+++ src/main.py
@@ -2,20 +2,28 @@ from f_recursive_static_to_private import r_static_to_private
 from f_recursive_generate_pages import r_generate_pages
 import os
 import shutil
+import sys
+
+basepath = "/"
+
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
 
 static_dir = "static"
-public_dir = "public"
+public_dir = "docs"
 content_dir = "content"
 
-def main():
 
+def main():
+    print(f"sys.argv is = {sys.argv}")
+    print(f"basepath = {basepath}")
     if os.path.exists(public_dir):
          shutil.rmtree(public_dir)
 
     r_static_to_private(static_dir, public_dir)
 
 
-    r_generate_pages(content_dir, 'template.html', public_dir)
+    r_generate_pages(basepath, content_dir, 'template.html', public_dir)
 
 
 
